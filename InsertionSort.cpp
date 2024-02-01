3e7a0f480ba124c6c7bc923cd97487eb14181555
S.M. Nabiul Islam <129619658+NabiulIslamNabil@users.noreply.github.com>
2024-02-02 02:28:09 +0600

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -20,17 +20,18 @@ void printArray(int arr[], int n){
 }
 
 void insertionSort(int arr[], int n){
-    cout<<"After Insertion Sort: ";
-    for(int i=0; i<n; i++){
-        int j = i;
-        while(j>0 && arr[j]<arr[j-1]){
-            int temp = arr[j-1];
-            arr[j-1] = arr[j];
-            arr[j] = temp;
-            j--;
+    cout<<"After insertion sort: ";
+    for(int i=1; i<n; i++){
+        int current = arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j]>current){
+            arr[j+1] = arr[j];
+            --j;
         }
+        arr[j+1] = current;
     }
 }
+
 int main(){
 
     int n = 10;
